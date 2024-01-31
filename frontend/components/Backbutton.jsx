3726b12862114933b59b354c97144932d05d22f7
Usha Nandhini <gushanandhini2004@gmail.com>
2024-01-31 19:54:11 +0530

    enough

--- frontend/components/Backbutton.jsx
+++ frontend/components/Backbutton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { BsArrowLeft } from 'react-icons/bs'
 
-const Backbutton = ({destination='/'}) => {
+const Backbutton = ({destination='/home'}) => {
   return (
     <div className='flex'>
         <Link to={destination} className='bg-sky-500 text-white py-1 px-4 rounded-lg w-fit'>
